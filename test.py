b55c33823590ab796ff1c2e58cc21767c52ab376
glawber <glawber@gmail.com>
2021-10-24 21:29:26 -0400

    Alteracao cabecalho descricao perfil 1

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline Devops - Glauber B. de Souza.")
+        self.assertRegex(result.data.decode(), "Pipeline Devops - Glauber de Souza.")
 
 
 if __name__ == "__main__":
